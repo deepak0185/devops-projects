91573bd0cbb34695dc237abe719db418a9f1e033
Deepak Negi <deepaknegi@Deepaks-MacBook-Pro.local>
2026-03-21 00:40:08 +0530

    loops add

--- First.py
+++ First.py
@@ -3,3 +3,9 @@ print ("updated code for learning git")
 print ("another update for learning git")
 print("this is the last update for learning git")
 print ("now we are going to learn about branches in git")
+
+#New feature added for learning branches in git
+print("/n--- Learning loops")
+for i in range(5):
+    print("This is loop iteration number", i)
+print("/n--- Learning functions")
